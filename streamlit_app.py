e6b0b974985dddc0d622ac10567a1a6574833a77
VishalP1015 <156264155+VishalP1015@users.noreply.github.com>
2024-05-07 07:39:09 -0700

    Update streamlit_app.py
    add fruity vice api in selecting ingredients piece of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+import requests
+
+
 # Write directly to the app
 #st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.title('My Parents New Healthy Diner')
@@ -36,10 +39,9 @@ ingredients_list = st.multiselect(
     , max_selections = 5
 )
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 if ingredients_list:
     #st.write(ingredients_list)
@@ -49,6 +51,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
